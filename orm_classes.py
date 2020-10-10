9fe8a446846ceb5be4b71f5eac5adb0968549215
megahomyak <g.megahomyak@gmail.com>
2020-10-10 16:33:19 +0500

    Made ORM class Order

---
+++ orm_classes.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+
+DeclarativeBase = declarative_base()
+
+
+class Order(DeclarativeBase):
+
+    id = Column(Integer, primary_key=True)
+
+    creator_vk_id = Column(Integer, nullable=False)
+    text = Column(String, nullable=False)
+
+    taker_vk_id = Column(Integer)
+
+    canceler_vk_id = Column(Integer)
+    cancellation_reason = Column(String)
+
+    earnings = Column(Integer)
+    earning_date = Column(DateTime)
