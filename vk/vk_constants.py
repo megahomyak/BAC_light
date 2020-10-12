10fdbe4b3d6fea4314c4ca02b7bf3b06519da0ed
megahomyak <g.megahomyak@gmail.com>
2020-10-12 17:43:54 +0500

    Now vk_constants.py takes secrets from the vk_secrets.ini

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -1,4 +1,12 @@
-SYMBOLS_LIMIT = 4096
+import configparser
 
-TOKEN = ""
-GROUP_ID = 123
+_secret_config = configparser.ConfigParser()
+_secret_config.read("vk/vk_secrets.ini")
+# vk_secrets.ini is not in git, so you need to create it
+
+
+TOKEN = _secret_config["SECRETS"]["token"]
+GROUP_ID = int(_secret_config["SECRETS"]["group_id"])
+
+
+SYMBOLS_LIMIT = 4096
