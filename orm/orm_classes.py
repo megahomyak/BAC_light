14e81b15eaefd2b2cdff31deaca796e2579500b7
megahomyak <g.megahomyak@gmail.com>
2020-10-16 02:48:26 +0500

    Properties of Order now can be used in the SQL queries

--- orm/orm_classes.py
+++ orm/orm_classes.py
@@ -1,9 +1,13 @@
 from sqlalchemy import Column, Integer, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
 
 DeclarativeBase = declarative_base()
 
 
+# noinspection PyMethodParameters
+# Because when the first argument is named `cls`, class is really
+# passed into it
 class Order(DeclarativeBase):
 
     __tablename__ = "orders"
@@ -21,14 +25,26 @@ class Order(DeclarativeBase):
     earnings = Column(Integer)
     earning_date = Column(DateTime)
 
-    @property
-    def is_taken(self):
+    @hybrid_property
+    def is_taken(self) -> bool:
         return self.taker_vk_id is not None
 
-    @property
-    def is_canceled(self):
+    @is_taken.expression
+    def is_taken(cls):
+        return cls.taker_vk_id.isnot(None)
+
+    @hybrid_property
+    def is_canceled(self) -> bool:
         return self.canceler_vk_id is not None
 
-    @property
-    def is_paid(self):
+    @is_canceled.expression
+    def is_canceled(cls):
+        return cls.canceler_vk_id.isnot(None)
+
+    @hybrid_property
+    def is_paid(self) -> bool:
         return self.earnings is not None
+
+    @is_paid.expression
+    def is_paid(cls):
+        return cls.earnings.isnot(None)
