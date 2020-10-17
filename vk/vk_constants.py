2dda654ea960eba2194242308768047c37317aa8
megahomyak <g.megahomyak@gmail.com>
2020-10-17 17:27:00 +0500

    Now .ini files is read with the utf-8 encoding

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -1,7 +1,7 @@
 import configparser
 
 _secret_config = configparser.ConfigParser()
-_secret_config.read("vk/vk_secrets.ini")
+_secret_config.read("vk/vk_secrets.ini", "utf-8")
 # vk_secrets.ini is not in git, so you need to create it
 
 TOKEN = _secret_config["SECRETS"]["token"]
@@ -12,7 +12,7 @@ EMPLOYEES_CHAT_PEER_ID = int(
 
 
 _constants_config = configparser.ConfigParser()
-_constants_config.read("vk/vk_constants.ini")
+_constants_config.read("vk/vk_constants.ini", "utf-8")
 
 SYMBOLS_PER_MESSAGE = int(_constants_config["MESSAGES"]["symbols_limit"])
 _help_message_ending = _constants_config["HELP_MESSAGE"]["ending"]
