f33c6eda0b2725b3b5a6310bbdc68898905933c8
megahomyak <g.megahomyak@gmail.com>
2020-10-17 16:51:58 +0500

    Renamed vk_constants.ini to vk_secrets.ini

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -1,8 +1,8 @@
 import configparser
 
 _secret_config = configparser.ConfigParser()
-_secret_config.read("vk/vk_constants.ini")
-# vk_constants.ini is not in git, so you need to create it
+_secret_config.read("vk/vk_secrets.ini")
+# vk_secrets.ini is not in git, so you need to create it
 
 
 TOKEN = _secret_config["SECRETS"]["token"]
