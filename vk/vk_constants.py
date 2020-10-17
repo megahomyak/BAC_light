6965bf1b4591c0ffad07ba1d74aeed6b809cc9fe
megahomyak <g.megahomyak@gmail.com>
2020-10-17 17:00:19 +0500

    Made vk_constants.ini and also made a constant for help message ending (I will make command for help later)

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -4,12 +4,15 @@ _secret_config = configparser.ConfigParser()
 _secret_config.read("vk/vk_secrets.ini")
 # vk_secrets.ini is not in git, so you need to create it
 
-
 TOKEN = _secret_config["SECRETS"]["token"]
 GROUP_ID = int(_secret_config["SECRETS"]["group_id"])
-
-
-SYMBOLS_PER_MESSAGE = 4096
 EMPLOYEES_CHAT_PEER_ID = int(
     _secret_config["EMPLOYEES"]["employees_chat_peer_id"]
 )
+
+
+_constants_config = configparser.ConfigParser()
+_constants_config.read("vk/vk_constants.ini")
+
+SYMBOLS_PER_MESSAGE = int(_constants_config["MESSAGES"]["symbols_limit"])
+HELP_MESSAGE_ENDING = _constants_config["HELP_MESSAGE"]["ending"]
