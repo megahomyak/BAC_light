84237cd20453f488413b3f55c90a13486d53b0ff
megahomyak <g.megahomyak@gmail.com>
2020-10-25 13:35:30 +0500

    Comment fix lol

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -2,7 +2,7 @@ import configparser
 
 _secret_config = configparser.ConfigParser()
 _secret_config.read("vk/vk_secrets.ini", "utf-8")
-# vk_secrets.ini is not in git, so you need to create it
+# vk_secrets.ini is not in the git, so you need to create it
 
 TOKEN = _secret_config["SECRETS"]["token"]
 GROUP_ID = int(_secret_config["SECRETS"]["group_id"])
