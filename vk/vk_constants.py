cbe11945b5c87716cf33685fd5cd5c101d5b3040
megahomyak <g.megahomyak@gmail.com>
2020-11-05 17:45:08 +0500

    vk_secrets.ini/vk_constants.ini sectors is now marked with comments

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -1,5 +1,8 @@
 import configparser
 
+
+# Unpacking vk_secrets.ini
+
 _secret_config = configparser.ConfigParser()
 _secret_config.read("vk/vk_secrets.ini", "utf-8")
 # vk_secrets.ini is not in the git, so you need to create it
@@ -10,6 +13,8 @@ EMPLOYEES_CHAT_PEER_ID = int(
     _secret_config["EMPLOYEES"]["employees_chat_peer_id"]
 )
 
+
+# Unpacking vk_constants.ini
 
 _constants_config = configparser.ConfigParser()
 _constants_config.read("vk/vk_constants.ini", "utf-8")
