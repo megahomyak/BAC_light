d37f294eadc97450d99243b8e010f63895d393c8
megahomyak <g.megahomyak@gmail.com>
2020-11-10 19:37:01 +0500

    Sending a notification when unable to send a callback message to user

--- __main__.py
+++ __main__.py
@@ -4,11 +4,12 @@ import traceback
 from typing import NoReturn, Optional, List, Tuple, Dict, Callable
 
 import aiohttp
+import simple_avk
 import simplest_logger
-from simple_avk import SimpleAVK
 
 import lexer.exceptions
 import lexer.generators
+from enums import GrammaticalCases
 from handlers.handler_helpers import HandlerHelpers
 from handlers.handlers import Handlers, HandlingResult
 from lexer.enums import IntTypes
@@ -22,6 +23,7 @@ from lexer.lexer_implementations import (
 from orm import db_apis
 from orm.enums import DBSessionChanged
 from vk import vk_constants
+from vk.enums import Sex
 from vk.vk_related_classes import Message
 from vk.vk_worker import VKWorker
 
@@ -37,6 +39,9 @@ class MainLogic:
         self.vk_worker = vk_worker
         self.logger = logger
         self.log_command_parsing_errors = log_command_parsing_errors
+        self.get_tag_from_vk_user_dataclass = (
+            handlers.helpers.get_tag_from_vk_user_dataclass
+        )
         self.commands: Tuple[Command, ...] = (
             *lexer.generators.get_getter_commands_for_common_orders(
                 ("заказы",), ("orders",), "заказы", handlers.get_orders
@@ -377,10 +382,9 @@ class MainLogic:
                     ]
 
     async def handle_command(
-            self, vk_message_info: dict,
+            self, current_chat_peer_id: int, command: str,
+            vk_message_info: dict,
             constant_context: ConstantContext) -> List[Message]:
-        command = vk_message_info["text"][1:]  # Cutting /
-        current_chat_peer_id = vk_message_info["peer_id"]
         error_args_amount = 0
         for command_ in self.commands:
             try:
@@ -453,11 +457,77 @@ class MainLogic:
         ]
 
     async def reply_to_vk_message(
-            self, message_info: dict,
+            self, current_chat_peer_id: int, command: str, message_info: dict,
             constant_context: ConstantContext) -> None:
-        await self.vk_worker.multiple_reply(
-            *await self.handle_command(message_info, constant_context)
+        done_replies = await self.vk_worker.multiple_reply(
+            *await self.handle_command(
+                current_chat_peer_id, command, message_info, constant_context
+            )
         )
+        ids_of_people_who_blacklisted_the_bot = []
+        for reply in done_replies:
+            # noinspection PyUnresolvedReferences
+            # because IDK why when I write this code below, which starts with a
+            # [^], my IDE thinks that reply.exception is None and it can't have
+            # a __class__ property EVEN WHEN None HAVE A __class__ PROPERTY!!!
+            if (
+                reply.exception.__class__ is simple_avk.MethodError
+                and
+                # Can't send messages to user without permission
+                reply.exception.error_code == 901
+            ):
+                ids_of_people_who_blacklisted_the_bot.append(
+                    reply.message.peer_id
+                )
+            else:
+                if reply.exception is not None:  # [^] For the comment above
+                    raise reply.exception
+        if ids_of_people_who_blacklisted_the_bot:
+            users = [
+                await (
+                    self.managers_container
+                    .users_manager
+                    .get_user_info_by_id(id_, GrammaticalCases.GENITIVE)
+                )
+                for id_ in ids_of_people_who_blacklisted_the_bot
+            ]
+            tags = [
+                self.get_tag_from_vk_user_dataclass(user)
+                for user in users
+            ]
+            tags_str = " и ".join(
+                [
+                    i
+                    for i in (
+                        ", ".join(tags[:-1]),
+                        tags[-1]
+                    )
+                    if i
+                ]
+            )
+            if len(users) == 1:
+                if users[0].sex == Sex.MALE:
+                    them_he_or_she_word = "него"
+                    they_he_or_she_word = "он"
+                    wrote_word = "писал"
+                else:
+                    them_he_or_she_word = "неё"
+                    they_he_or_she_word = "она"
+                    wrote_word = "писала"
+            else:
+                them_he_or_she_word = "них"
+                they_he_or_she_word = "они"
+                wrote_word = "писали"
+            await self.vk_worker.reply(
+                Message(
+                    (
+                        f"Невозможно отправить сообщения для {tags_str}, "
+                        f"потому что {they_he_or_she_word} никогда боту не "
+                        f"{wrote_word} или бот у {them_he_or_she_word} в ЧС."
+                    ),
+                    current_chat_peer_id
+                )
+            )
 
     async def future_done_callback(
             self, peer_id: int, text: str,
@@ -505,7 +575,9 @@ class MainLogic:
             if text.startswith("/"):
                 text = text[1:]  # Cutting /
                 asyncio.create_task(
-                    self.reply_to_vk_message(message_info, constant_context)
+                    self.reply_to_vk_message(
+                        peer_id, text, message_info, constant_context
+                    )
                 ).add_done_callback(
                     lambda future: asyncio.create_task(
                         self.future_done_callback(
@@ -518,7 +590,7 @@ class MainLogic:
 async def main():
     async with aiohttp.ClientSession() as aiohttp_session:
         vk_worker = VKWorker(
-            SimpleAVK(
+            simple_avk.SimpleAVK(
                 aiohttp_session,
                 vk_constants.TOKEN,
                 vk_constants.GROUP_ID
